aa57f0db443316fd7263a9fcf4632932cc2d04d4
miko <bo@bro.com>
2017-12-14 15:33:24 +0300

    Added examples and fixed tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='SQLAlchemy-serializer',
       version='0.2',
-      description='SQLAlchemy-serializer mixin',
+      description='Mixin for SQLAlchemy-models serialization without pain',
       author='Y-Bro',
       url='https://github.com/n0nSmoker/SQLAlchemy-serializer.git',
       keywords=['sqlalchemy', 'serialize', 'to_dict'],
