ec9855dac513c3d5707b1bd199352f271aa5211d
miko <bo@bro.com>
2017-12-20 19:06:09 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 
 setup(name='SQLAlchemy-serializer',
-      version='0.3',
+      version='0.4',
       description='Mixin for SQLAlchemy-models serialization without pain',
       author='Y-Bro',
       url='https://github.com/n0nSmoker/SQLAlchemy-serializer.git',
-      keywords=['sqlalchemy', 'serialize', 'to_dict'],
+      keywords=['sqlalchemy', 'serialize', 'to_dict', 'JSON'],
       packages=['sqlalchemy_serializer'],
       install_requires=[
         'SQLAlchemy',
