abcd0dca53aa8b37617b47b43afcd54ce6b60996
miko <bro@bro.com>
2018-01-16 14:23:28 +0300

    Fixed lib import issue

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 setup(name='SQLAlchemy-serializer',
-      version='0.7',
+      version='0.8',
       description='Mixin for SQLAlchemy-models serialization without pain',
       author='Y-Bro',
       url='https://github.com/n0nSmoker/SQLAlchemy-serializer.git',
       keywords=['sqlalchemy', 'serialize', 'to_dict', 'JSON'],
-      packages=['sqlalchemy_serializer'],
+      packages=['sqlalchemy_serializer', 'sqlalchemy_serializer.lib'],
       install_requires=[
         'SQLAlchemy',
         'Flask-Babel',
