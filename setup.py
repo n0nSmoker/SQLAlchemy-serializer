93b649e3c3c803875a1ad1dd61faafc5eaf05525
miko <stoprobots@gmail.com>
2019-04-22 10:58:34 +0300

    Removed long description

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from distutils.core import setup
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='SQLAlchemy-serializer',
     version='1.1.3',
     description='Mixin for SQLAlchemy-models serialization without pain',
-    long_description=long_description,
-    long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
     author='Y-Bro',
     url='https://github.com/n0nSmoker/SQLAlchemy-serializer.git',
     keywords=['sqlalchemy', 'serialize', 'to_dict', 'JSON'],
