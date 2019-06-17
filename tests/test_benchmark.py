aedb002bc05b5eaa2d1e836f7c341fb9ea4f6d4a
miko <stoprobots@gmail.com>
2019-06-18 02:19:02 +0300

    Added benchmark test for experiments

---
+++ tests/test_benchmark.py
@@ -0,0 +1,15 @@
+# from .models import FlatModel, NestedModel
+#
+#
+# def test_flat_instance(get_instance):
+#     """
+#     Performs 5000 serialisations
+#     Should be called with --profile flag (pytest-profile)
+#     :param get_instance:
+#     :return:
+#     """
+#    i = get_instance(NestedModel, model_id=get_instance(FlatModel).id)
+#
+#     for _ in range(5000):
+#         i.to_dict()
+
