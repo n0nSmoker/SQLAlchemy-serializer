f713c92a843927e04b1f769a93661a1679888d12
miko <stoprobots@gmail.com>
2019-10-22 12:27:50 +0300

    Changed logging level in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,12 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARN,
     format='%(asctime)s | %(name)s > %(message)s',
     datefmt='%d.%m.%Y %H:%M:%S'
 )
 
 
 logger = logging.getLogger('serializer')
-logger.setLevel(logging.WARN)
+logger.setLevel(logging.INFO)
 
