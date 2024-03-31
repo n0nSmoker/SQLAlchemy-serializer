89318917276613f3bcbb4622e3426e2dcad9e48b
Yuri Boiko <stoprobots@gmail.com>
2024-03-31 15:16:01 +0200

    Proper typehints in timzones logic

--- sqlalchemy_serializer/lib/timezones.py
+++ sqlalchemy_serializer/lib/timezones.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 
 
-def to_local_time(dt, tzinfo):
+def to_local_time(dt: datetime, tzinfo) -> datetime:
     normalized = dt.astimezone(tzinfo)
     return normalized.replace(tzinfo=None)
 
 
-def format_dt(dt, tpl=None):
+def format_dt(dt: datetime, tpl=None) -> str:
     if not tpl:
         return dt.isoformat()
     return dt.strftime(tpl)
